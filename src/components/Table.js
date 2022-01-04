4cefc2be512eb61bcf60accb85c67794bdb3fe71
fariasao <enzofarias2010@gmail.com>
2022-01-04 02:04:37 -0300

    requisito 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,20 +6,94 @@ function Table() {
   const { planets, getPlanets } = useContext(StarWarsContext);
   const [NameFilter, setNameFilter] = useState('');
 
+  const [filterType, setFilterType] = useState('population');
+  const [filterComparison, setFilterComparison] = useState('maior que');
+  const [filterValue, setFilterValue] = useState(0);
+
+  const [planetsList, setPlanetsList] = useState([]);
+
   // useEffect do DidMount
   useEffect(() => {
     getPlanets();
   }, [getPlanets]);
 
+  // useEffect para DidUpdate
+  // executa td vez que o parametro alterar
+  useEffect(() => {
+    setPlanetsList(planets);
+  }, [planets]);
+
+  // ref https://github.com/tryber/sd-015-b-project-starwars-planets-search/pull/2
+  function handleClick() {
+    if (filterComparison === 'maior que') {
+      setPlanetsList(planetsList
+        .filter((planet) => (Number(planet[filterType]) > filterValue)));
+    } else if (filterComparison === 'menor que') {
+      setPlanetsList(planetsList
+        .filter((planet) => (Number(planet[filterType]) < filterValue)));
+    } else {
+      setPlanetsList(planetsList
+        .filter((planet) => (Number(planet[filterType]) === Number(filterValue))));
+    }
+  }
+
   return (
     <>
-      <input
-        type="text"
-        placeholder="buscar o planeta..."
-        data-testid="name-filter"
-        value={ NameFilter }
-        onChange={ (event) => setNameFilter(event.target.value) }
-      />
+      <form>
+        <input
+          type="text"
+          placeholder="bota o planeta..."
+          data-testid="name-filter"
+          value={ NameFilter }
+          onChange={ (event) => setNameFilter(event.target.value) }
+        />
+      </form>
+
+      <form>
+        {/* primeiro filtro de colunas */}
+        <select
+          name="columnFilter"
+          id="columnFilter"
+          data-testid="column-filter"
+          onChange={ (event) => setFilterType(event.target.value) }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+
+        {/* segundo filtro de comparacao */}
+        <select
+          name="comparisonFilter"
+          id="comparisonFilter"
+          data-testid="comparison-filter"
+          onChange={ (event) => setFilterComparison(event.target.value) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+
+        {/* terceiro filtro de valor */}
+        <input
+          type="number"
+          placeholder="valor"
+          data-testid="value-filter"
+          value={ filterValue }
+          onChange={ (event) => setFilterValue(event.target.value) }
+        />
+
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleClick }
+        >
+          Procurar
+        </button>
+      </form>
+
       <table className="table-container">
         <thead>
           <tr className="table-header">
@@ -39,7 +113,8 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {planets.filter((planet) => (planet.name.toLowerCase().includes(NameFilter)))
+          {planetsList
+            .filter((planet) => (planet.name.toLowerCase().includes(NameFilter)))
             .map((planet) => (
               <tr key={ planet.name } className="table-body">
                 <td>{planet.name}</td>
