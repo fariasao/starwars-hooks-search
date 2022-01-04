8ab5e4b2a915b251f2a1d61b3891aa5a5cbfa387
fariasao <enzofarias2010@gmail.com>
2022-01-04 01:39:38 -0300

    requisito 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 import './Table.css';
 
 function Table() {
   const { planets, getPlanets } = useContext(StarWarsContext);
+  const [NameFilter, setNameFilter] = useState('');
 
   // useEffect do DidMount
   useEffect(() => {
@@ -11,44 +12,54 @@ function Table() {
   }, [getPlanets]);
 
   return (
-    <table className="table-container">
-      <thead>
-        <tr className="table-header">
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface_water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {planets.map((planet) => (
-          <tr key={ planet.name } className="table-body">
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+    <>
+      <input
+        type="text"
+        placeholder="buscar o planeta..."
+        data-testid="name-filter"
+        value={ NameFilter }
+        onChange={ (event) => setNameFilter(event.target.value) }
+      />
+      <table className="table-container">
+        <thead>
+          <tr className="table-header">
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface_water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {planets.filter((planet) => (planet.name.toLowerCase().includes(NameFilter)))
+            .map((planet) => (
+              <tr key={ planet.name } className="table-body">
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
